bdd7e30172492902366a1216895d436668c8e076
Abbosbek Ustaboyev Botirjonovich <102945540+AbbosbekMobiler@users.noreply.github.com>
2022-05-27 09:08:51 +0500

    firs commit

--- app/src/main/java/com/example/textfieldsbuttonsnackbarcompose/MainActivity.kt
+++ app/src/main/java/com/example/textfieldsbuttonsnackbarcompose/MainActivity.kt
@@ -3,41 +3,57 @@ package com.example.textfieldsbuttonsnackbarcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.textfieldsbuttonsnackbarcompose.ui.theme.TextFieldsButtonSnackBarComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TextFieldsButtonSnackBarComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+            val scaffoldState = rememberScaffoldState()
+            var textFieldState by remember {
+                mutableStateOf("")
+            }
+
+            var scope = rememberCoroutineScope()
+            Scaffold(
+                Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 30.dp)
                 ) {
-                    Greeting("Android")
+                    TextField(value = textFieldState, label = {
+                        Text(text = "Enter Your Name")
+                    },
+                    onValueChange = {
+                        textFieldState = it
+                    },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth())
+                    Spacer(modifier = Modifier.height(60.dp))
+                    Button(onClick = {
+                        scope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
+                        }
+                    }) {
+                        Text(text = "Pls greet me")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TextFieldsButtonSnackBarComposeTheme {
-        Greeting("Android")
     }
 }
